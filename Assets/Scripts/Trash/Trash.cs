68eb110baaf3ff283f06894f6a50a5449aabd6c9
F8Code <hubgod@onet.pl>
2025-11-02 17:52:55 +0100

    Trash scripts setup

--- Assets/Scripts/Trash/Trash.cs
+++ Assets/Scripts/Trash/Trash.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Trash : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    TrashData _data;
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(TrashData data)
     {
-        
+        _data = data;
+        ApplyData();
+    }
+    
+    private void ApplyData()
+    {
+        /*// Ustaw model i materiały
+        if (_data.modelPrefab != null)
+        {
+            // Możesz wstawić model jako dziecko
+            var model = Instantiate(_data.modelPrefab, transform);
+        }
+
+        var renderer = GetComponentInChildren<Renderer>();
+        if (renderer && _data.material != null)
+            renderer.material = _data.material;
+
+        var collider = GetComponent<Collider>();
+        if (collider && _data.physicsMaterial != null)
+            collider.material = _data.physicsMaterial;
+
+        // Dodatkowe właściwości typu specjalnego
+        _data.OnCollected(); // przykład użycia logiki polimorficznej*/
     }
 }
