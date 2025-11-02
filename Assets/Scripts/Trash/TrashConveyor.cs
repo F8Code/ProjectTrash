a7e0cdc4bbff5c905892450847f5539d858a2505
F8Code <hubgod@onet.pl>
2025-11-02 15:53:04 +0100

    Idk what Im pushing

--- Assets/Scripts/Trash/TrashConveyor.cs
+++ Assets/Scripts/Trash/TrashConveyor.cs
@@ -1,16 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashConveyor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    const float CONVEYOR_SPEED_MODIFIER = 0.1f;
+
+    [Header("Conveyor settings")]
+    [Tooltip("Speed at which trash moves in the conveyor local forward direction")]
+    [SerializeField, Range(0.1f, 5f)] float _trashMovementSpeed = 1f;
+    [Tooltip("If enabled, disables gravity effect of trash while on the conveyor")]
+    [SerializeField] bool _disableTrashGravity = false;
+    [Tooltip("Extra fling speed applied to trash when it leaves the conveyor")]
+    [SerializeField, Range(0f, 2f)] float _trashFlingSpeed = 2f;
+
+    HashSet<Rigidbody> _trash = new();
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.layer != GameConstants.Layer.Trash)
+            return;
+
+        Rigidbody trash = other.GetComponent<Rigidbody>();
+
+        if (_disableTrashGravity)
+            trash.isKinematic = true;
+
+        trash.MovePosition(trash.position);
+
+        _trash.Add(trash);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach(Rigidbody trash in _trash)
+            trash.MovePosition(trash.position + transform.forward * _trashMovementSpeed * Time.fixedDeltaTime * CONVEYOR_SPEED_MODIFIER);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != GameConstants.Layer.Trash)
+            return;
+
+        Rigidbody trash = other.GetComponent<Rigidbody>();
+
+        if (_disableTrashGravity)
+            trash.isKinematic = false;
+
+        trash.linearVelocity += transform.forward * _trashFlingSpeed;
+
+        _trash.Remove(trash);
     }
 }
