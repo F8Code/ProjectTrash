b6330da734e05fd79ddae7c7f88be296e73ddc5b
Ahmed Afifi Abdou <aafifi1988@icloud.com>
2025-11-07 21:50:45 +0200

    -Update

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    InputSystem_Actions _input;
+    private InputSystem_Actions _input;
 
     internal InputSystem_Actions.PlayerActions PlayerActions { get; private set; }
     internal InputSystem_Actions.UIActions UIActions { get; private set; }
 
     public static InputManager Instance { get; private set; }
 
-    void Awake()
+    private void Awake()
     {
         if (Instance != null && Instance != this)
         {
@@ -23,17 +23,30 @@ public class InputManager : MonoBehaviour
         InitializeActions();
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
         _input?.Enable();
     }
 
-    //void OnDisable()
-    //{
-    //    _input?.Disable();
-    //}
+    private void OnDisable()
+    {
+        _input?.Disable();
+    }
 
-    void InitializeActions()
+    private void OnDestroy()
+    {
+        if (_input != null)
+        {
+            _input.Disable();
+            _input.Dispose();
+            _input = null;
+        }
+
+        if (Instance == this)
+            Instance = null;
+    }
+
+    private void InitializeActions()
     {
         PlayerActions = _input.Player;
         UIActions = _input.UI;
